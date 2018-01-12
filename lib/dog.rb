b44b3d719a1f231cdbefb814d6b39701f1b65930
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-01-12 20:50:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,11 @@
 class Dog
-  def initialize(name, breed)
-    @this_dogs_name = dogs_name
-    @this_dogs_breed = dogs_breed
+
+  def name
+    @name
   end
+
+  def breed
+    @breed
+  end
+  
 end
